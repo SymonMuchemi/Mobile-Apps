63af02c5e18384ca21773ea0b81c137e67f03b41
SymonMuchemi <simonkenya846@gmail.com>
2023-09-25 10:06:40 +0300

    Assigning IDs to widgets

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView textView;
+    public EditText firstname;
+    public EditText lastname;
+    public Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Binding
+
     }
 }
