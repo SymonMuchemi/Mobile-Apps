2937134d1aed90f9b73fcfc85f2702bece25985e
SymonMuchemi <simonkenya846@gmail.com>
2023-09-25 10:37:02 +0300

    onClickListener

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public TextView textView;
@@ -19,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Binding
+        textView = findViewById(R.id.textView);
+        firstname = findViewById(R.id.firstname);
+        lastname = findViewById(R.id.lastname);
+        btn = findViewById(R.id.btn);
 
+        // adding a click listener
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputFirstName = String.valueOf(firstname.getText());
+                String inputLastName = String.valueOf(lastname.getText());
+                Toast.makeText(MainActivity.this, "Hello " + inputLastName + " " + inputLastName, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
